ce679639dc4f7023b2e0fe6956379e1ea032bf75
sync <sync@someplace.com>
2019-06-19 03:48:55 +0000

    from build definition

--- Edison.Web/Kubernetes/prd/Deployment/Ingress_Custom/4-InstallCertManager.ps1
+++ Edison.Web/Kubernetes/prd/Deployment/Ingress_Custom/4-InstallCertManager.ps1
@@ -1,2 +1,2 @@
-helm install --name cert-manager stable/cert-manager --namespace kube-system --set ingressShim.defaultIssuerName=letsencrypt-prod --set ingressShim.defaultIssuerKind=ClusterIssuer --set rbac.create=false --set serviceAccount.create=false
+helm install --name cert-manager stable/cert-manager --namespace kube-system --set ingressShim.defaultIssuerName=letsencrypt-prod --set ingressShim.defaultIssuerKind=ClusterIssuer --set rbac.create=true --set serviceAccount.create=true
 #helm delete cert-manager --purge
